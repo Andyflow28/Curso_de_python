a8a4b51447d7dabb3ebd612abbe3ff9439db39b4
Andres Flores <joseguerreroaf@gmail.com>
2025-04-20 09:38:36 -0400

    este es un commit

---
+++ hi.py
@@ -0,0 +1,13 @@
+# hola mundo
+print('hola mundo')
+"""esta es un 
+comentario multilinea"""
+
+"""Tipos de datos"""
+
+print(type(3))
+print(type(3.5))
+print(type(3- 2j))
+print(type(True))
+print(type("Datos"))
+
